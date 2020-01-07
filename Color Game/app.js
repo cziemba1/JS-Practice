5ca9d1f37ade8e5a5424ea213fe1231f8fb9d835
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-07 00:26:36 -0300

    Add files via upload

--- Color Game/app.js
+++ Color Game/app.js
@@ -1,21 +1,16 @@
 const color = document.querySelector(".color");
-const newcolor = document.querySelector(".new-color");
+const reset = document.querySelector(".reset");
 const easy = document.querySelector(".easy");
 const hard = document.querySelector(".hard");
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 255)",
-]
+let colors = generateColor(6);
 const squares = document.querySelectorAll(".square");
-let pickedColor = colors[4];
+// let pickedColor = colors[4];
 const newContent = document.querySelector(".new-content");
 const header = document.querySelector(".header");
+let pickedColor = pickColor();
+let numSquares = 6;
 
-color.textContent = pickedColor;
+color.textContent = pickColor();
 
 
 for (let i = 0; i < squares.length; i++) {
@@ -23,12 +18,10 @@ for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function () {
         let clickedColor = this.style.backgroundColor;
         if (clickedColor === pickedColor) {
-            for (let i = 0; i < squares.length; i++) {
-                squares[i].style.backgroundColor = clickedColor;
-            }
+            changeColor(clickedColor);
             newContent.textContent = "YOU WIN";
-            header.style.backgroundColor = pickedColor;
-
+            header.style.backgroundColor = clickedColor;
+            reset.textContent = "PLAY AGAIN?"
         } else {
             this.style.backgroundColor = "#000000";
             newContent.textContent = "Try Again";
@@ -36,11 +29,73 @@ for (let i = 0; i < squares.length; i++) {
     })
 }
 
+reset.addEventListener("click", function () {
+    colors = generateColor(numSquares);
+    pickedColor = pickColor();
+    color.textContent = pickColor();
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        ;
+    }
+    reset.textContent = "NEW COLORS";
+    header.style.backgroundColor = "#567ef7";
+})
 
+easy.addEventListener("click", function () {
+    easy.classList.toggle("selected");
+    hard.classList.remove("selected");
+    numSquares = 3;
+    colors = generateColor(numSquares);
+    pickedColor = pickColor();
+    color.textContent = pickColor();
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
 
-// function generateColor() {
-//     return Math.floor(Math.random() * 256);
-// }
+hard.addEventListener("click", function () {
+    hard.classList.toggle("selected");
+    easy.classList.remove("selected");
+    numSquares = 6;
+    colors = generateColor(numSquares);
+    pickedColor = pickColor();
+    color.textContent = pickColor();
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
+
+function changeColor(color) {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateColor(num) { // num --> amount of colors/items in the array
+    let newArray = [];
+    for (let i = 0; i < num; i++) {
+        newArray.push(randomColor())
+    }
+    return newArray;
+}
+
+function randomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`
+
+}
 
 // newcolor.addEventListener("click", function () {
 //     color.textContent = `${generateColor()}, ${generateColor()}, ${generateColor()}`;
