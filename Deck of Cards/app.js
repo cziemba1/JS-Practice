0f9fd47aacbc3edd148255852b9ab5f394d6bd00
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-15 21:39:27 -0300

    Practica de "this"

--- Deck of Cards/app.js
+++ Deck of Cards/app.js
@@ -1,4 +1,4 @@
-function makeDeck() {
+/*function makeDeck() {
     const deck = [];
     const suits = ["hearts", "diamonds", "spades", "clubs"];
     const values = "2,3,4,5,6,7,8,9,10,J,Q,K,A";
@@ -11,4 +11,48 @@ function makeDeck() {
         }
     }
     return deck;
+}*/
+//Trabajando con objetos y "this"
+
+const myDeck = {
+    deck: [],
+    suits: ["hearts", "diamonds", "spades", "clubs"],
+    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+    drawCards: [],
+    startDeck() {
+        const {
+            deck,
+            suits,
+            values
+        } = this;
+        for (let value in values.split(",")) {
+            for (let suit in suits) {
+                deck.push({
+                    value,
+                    suit
+                })
+            }
+        }
+    },
+    drawCard() {
+        const card = this.deck.pop();
+        this.drawCards.push(card);
+        return card;
+    },
+    drawMultiple(numCards) {
+        const cards = [];
+        for (let i = 0; i < numCards; i++) {
+            cards.push(this.drawCard());
+        }
+        return cards;
+    },
+    suffle() {
+        const {
+            deck
+        } = this;
+        for (let i = deck.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+    }
 }
