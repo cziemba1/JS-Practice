3b290362ba35e93fab7b9a9583c38fa8a0757300
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-20 16:05:55 -0300

    Resolving/rejecting with values

--- Promises/app.js
+++ Promises/app.js
@@ -1,17 +1,47 @@
-const willGetYouADog = () => {
+// const willGetYouADog = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const rand = Math.random();
+//             if (rand < 0.5) {
+//                 resolve();
+//             } else {
+//                 reject();
+//             }
+//         }, 3000);
+//     });
+// }
+// willGetYouADog().then(() => { // este codigo correra solamente si resolve
+//     console.log("Yay we have a dog");
+// }).catch(() => { //este codigo correra solamente si reject
+//     console.log("No dog :(");
+// })
+
+const fakeRequest = (url) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            const rand = Math.random();
-            if (rand < 0.5) {
-                resolve();
+            const pages = {
+                "/users": [
+                    { id: 1, username: "Bilbo" },
+                    { id: 2, username: "Esmeralda" }
+                ],
+                "/about": "This is the about page"
+            };
+            const data = pages[url];
+            if (data) {
+                resolve({ status: 202, data });
             } else {
-                reject();
+                reject({ status: 404 });
             }
-        }, 3000);
+        }
+            , 3000);
     });
-}
-willGetYouADog().then(() => { // este codigo correra solamente si resolve
-    console.log("Yay we have a dog");
-}).catch(() => { //este codigo correra solamente si reject
-    console.log("No dog :(");
+};
+
+fakeRequest("/about").then((res) => {
+    console.log("Status code: ", res.status);
+    console.log("Data: ", res.data);
+    console.log("REQUESTED WORKED");
+}).catch((res) => {
+    console.log(res.status);
+    console.log("REQUESTED FAILED");
 })
