9487648ce51b0d1ceb741101f2fc67d15c39b89a
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-20 15:38:31 -0300

    Return promises from functions

--- Promises/app.js
+++ Promises/app.js
@@ -1,3 +1,17 @@
-const willGetYouADog = new Promise((resolve, rejected) => {
-
+const willGetYouADog = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const rand = Math.random();
+            if (rand < 0.5) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, 3000);
+    });
+}
+willGetYouADog().then(() => { // este codigo correra solamente si resolve
+    console.log("Yay we have a dog");
+}).catch(() => { //este codigo correra solamente si reject
+    console.log("No dog :(");
 })
