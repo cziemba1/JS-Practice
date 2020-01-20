9ae83bcd86d5c1085178336aa45cece270bb52c7
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-20 16:33:45 -0300

    Promise chaining

--- Promises/app.js
+++ Promises/app.js
@@ -20,11 +20,29 @@ const fakeRequest = (url) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             const pages = {
-                "/users": [
-                    { id: 1, username: "Bilbo" },
-                    { id: 2, username: "Esmeralda" }
+                '/users': [
+                    { id: 1, username: 'Bilbo' },
+                    { id: 5, username: 'Esmerelda' }
                 ],
-                "/about": "This is the about page"
+                '/users/1': {
+                    id: 1,
+                    username: 'Bilbo',
+                    upvotes: 360,
+                    city: 'Lisbon',
+                    topPostId: 454321
+                },
+                '/users/5': {
+                    id: 5,
+                    username: 'Esmerelda',
+                    upvotes: 571,
+                    city: 'Honolulu'
+                },
+                '/posts/454321': {
+                    id: 454321,
+                    title:
+                        'Ladies & Gentlemen, may I introduce my pet pig, Hamlet'
+                },
+                '/about': 'This is the about page!'
             };
             const data = pages[url];
             if (data) {
@@ -32,16 +50,33 @@ const fakeRequest = (url) => {
             } else {
                 reject({ status: 404 });
             }
-        }
-            , 3000);
+        }, 1000);
     });
 };
 
-fakeRequest("/about").then((res) => {
-    console.log("Status code: ", res.status);
-    console.log("Data: ", res.data);
-    console.log("REQUESTED WORKED");
-}).catch((res) => {
-    console.log(res.status);
-    console.log("REQUESTED FAILED");
-})
+// fakeRequest("/about").then((res) => {
+//     console.log("Status code: ", res.status);
+//     console.log("Data: ", res.data);
+//     console.log("REQUESTED WORKED");
+// }).catch((res) => {
+//     console.log(res.status);
+//     console.log("REQUESTED FAILED");
+// })
+
+fakeRequest("/users")
+    .then((res) => {
+        console.log(res);
+        const id = res.data[0].id; //id del primer usuario
+        return fakeRequest(`/users/${id}`)
+    })
+    .then((res) => {
+        console.log(res);
+        const postId = res.data.topPostId;
+        return fakeRequest(`/posts/${postId}`);
+    })
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log("oh no", err);
+    })
